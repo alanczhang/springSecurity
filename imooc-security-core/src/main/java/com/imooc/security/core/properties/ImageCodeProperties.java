8bf8480b5760ec06b5f166fb35520c716cb7abf9
alanczhang <81282101@qq.com>
2019-11-12 11:54:21 +0800

    4-8 图片验证码重构

--- imooc-security-core/src/main/java/com/imooc/security/core/properties/ImageCodeProperties.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/properties/ImageCodeProperties.java
@@ -8,14 +8,14 @@ package com.imooc.security.core.properties;
  *
  */
 public class ImageCodeProperties extends SmsCodeProperties {
-	
-	public ImageCodeProperties() {
-		setLength(4);
-	}
-	 
-	private int width = 67;
-	private int height = 23;
-	
+
+	private int width = 67;    //图片长度
+	private int height = 23;   //图片高度
+	private int length = 4;    //验证码长度
+	private int expireIn = 60; //失效时间
+
+	private String url;        //多个请求需要验证；逗号隔开
+
 	public int getWidth() {
 		return width;
 	}
@@ -28,5 +28,46 @@ public class ImageCodeProperties extends SmsCodeProperties {
 	public void setHeight(int height) {
 		this.height = height;
 	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	public int getExpireIn() {
+		return expireIn;
+	}
+	public void setExpireIn(int expireIn) {
+		this.expireIn = expireIn;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
 
 }
+
+
+	
+//	public ImageCodeProperties() {
+//		setLength(4);
+//	}
+//
+//	private int width = 67;
+//	private int height = 23;
+//
+//	public int getWidth() {
+//		return width;
+//	}
+//	public void setWidth(int width) {
+//		this.width = width;
+//	}
+//	public int getHeight() {
+//		return height;
+//	}
+//	public void setHeight(int height) {
+//		this.height = height;
+//	}
+
--- imooc-security-core/src/main/java/com/imooc/security/core/validate/code/image/ImageCode.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/validate/code/image/ImageCode.java
@@ -3,54 +3,80 @@
  */
 package com.imooc.security.core.validate.code.image;
 
+import com.imooc.security.core.validate.code.ValidateCode;
+
 import java.awt.image.BufferedImage;
 import java.time.LocalDateTime;
-
 
 /**
  * @author zhailiang
  *
  */
-public class ImageCode {
 
-	private BufferedImage image;//展示的图片
-	private String code;//生成的随机数，Session
-	private LocalDateTime expireTime;//过期时间
+public class ImageCode extends ValidateCode {
+
+	private BufferedImage image;
+
+	public ImageCode(BufferedImage image, String code, int expireIn){
+		super(code, expireIn);
+		this.image = image;
+	}
+
+	public ImageCode(BufferedImage image, String code, LocalDateTime expireTime){
+		super(code, expireTime);
+		this.image = image;
+	}
 
 	public BufferedImage getImage() {
 		return image;
 	}
+
 	public void setImage(BufferedImage image) {
 		this.image = image;
 	}
-	public String getCode() {
-		return code;
-	}
-	public void setCode(String code) {
-		this.code = code;
-	}
-	public LocalDateTime getExpireTime() {
-		return expireTime;
-	}
-	public void setExpireTime(LocalDateTime expireTime) {
-		this.expireTime = expireTime;
-	}
 
-	//是否过期
-	public boolean isExpried() {
-		return LocalDateTime.now().isAfter(expireTime);
-	}
-
-	public ImageCode(BufferedImage image, String code, LocalDateTime expireTime) {
-		this.image = image;
-		this.code = code;
-		this.expireTime = expireTime;
-	}
-
-	//多少秒过期（60秒）
-	public ImageCode(BufferedImage image, String code, int expireIn) {
-		this.image = image;
-		this.code = code;
-		this.expireTime = LocalDateTime.now().plusSeconds(expireIn);
-	}
 }
+
+//public class ImageCode extends ValidateCode {
+//
+//	private BufferedImage image;//展示的图片
+//	private String code;//生成的随机数，Session
+//	private LocalDateTime expireTime;//过期时间
+//
+//	public BufferedImage getImage() {
+//		return image;
+//	}
+//	public void setImage(BufferedImage image) {
+//		this.image = image;
+//	}
+//	public String getCode() {
+//		return code;
+//	}
+//	public void setCode(String code) {
+//		this.code = code;
+//	}
+//	public LocalDateTime getExpireTime() {
+//		return expireTime;
+//	}
+//	public void setExpireTime(LocalDateTime expireTime) {
+//		this.expireTime = expireTime;
+//	}
+//
+//	//是否过期
+//	public boolean isExpried() {
+//		return LocalDateTime.now().isAfter(expireTime);
+//	}
+//
+//	public ImageCode(BufferedImage image, String code, LocalDateTime expireTime) {
+//		this.image = image;
+//		this.code = code;
+//		this.expireTime = expireTime;
+//	}
+//
+//	//多少秒过期（60秒）
+//	public ImageCode(BufferedImage image, String code, int expireIn) {
+//		this.image = image;
+//		this.code = code;
+//		this.expireTime = LocalDateTime.now().plusSeconds(expireIn);
+//	}
+//}
